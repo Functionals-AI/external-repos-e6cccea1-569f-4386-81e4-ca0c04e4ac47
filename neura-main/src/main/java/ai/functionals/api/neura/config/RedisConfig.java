0d6253c3f92a93a555c3a78199713ad3d5997f44
suducode-functionals <suducode@functionals.ai>
2025-07-03 16:33:53 -0700

    Upload file via API: neura-main/src/main/java/ai/functionals/api/neura/config/RedisConfig.java

---
+++ neura-main/src/main/java/ai/functionals/api/neura/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package ai.functionals.api.neura.config;
+
+import ai.functionals.api.neura.model.rsp.LLMResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    
+    @Bean
+    public RedisTemplate<String, LLMResponse> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, LLMResponse> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(LLMResponse.class));
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> stringRedisTemplate2(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+} 
